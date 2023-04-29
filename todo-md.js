f224b901ad7defa0233ac82ab967d38f5fb7976d
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-04-28 23:48:29 -0500

    updated directory traversal to be recursive

--- todo-md.js
+++ todo-md.js
@@ -4,26 +4,29 @@ import writeMarkdownFile from './create-md.js';
 
 const directoryPath = './testDir';
 
-fs.readdir(directoryPath, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  files.forEach((file) => {
-    const filePath = path.join(directoryPath, file);
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      const lines = data.split('\n');
-
-      lines.forEach((line) => {
-        if (line.includes('// TODO:')) writeMarkdownFile(line)
+function traverseDirectory(dir) {
+  const files = fs.readdirSync(dir);
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      traverseDirectory(filePath);
+    } else {
+      console.log(filePath);
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+  
+        const lines = data.split('\n');
+  
+        lines.forEach((line) => {
+          if (line.includes('// TODO:')) writeMarkdownFile(line)
+        });
       });
-    });
+    }
   });
-});
+}
+
+traverseDirectory(directoryPath);
