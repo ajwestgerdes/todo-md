fc71b65731184283a81b2d71521ced3f0425e539
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-06-22 23:53:28 -0500

    messing around with basic todo structure

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+interface Todo {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newTodo = event.target.todo.value;
+    setTodos([...todos, { id: Date.now(), name: newTodo, completed: false }]);
+    event.target.todo.value = '';
+  };
+
+  const handleCheckTodo = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed =!updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" name="todo" placeholder="Add new todo" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={todo.id}>
+            {todo.name}
+            <input type="checkbox" onChange={() => handleCheckTodo(index)} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList />
     </div>
   );
 }
